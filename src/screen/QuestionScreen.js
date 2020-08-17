3047a7875646c449709a8f9fc3762e11b6d5883e
Daumantas <like.dmt@gmail.com>
2020-08-17 11:56:31 +0300

    Updated question style sheet

--- src/screen/QuestionScreen.js
+++ src/screen/QuestionScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Pressable } from "react-native";
 import shuffle from '../helper/Shuffle';
 
 const QuestionScreen = (props) => {
@@ -19,11 +19,27 @@ const QuestionScreen = (props) => {
     }, [])
 
     return (
-        <View>
-            <Text>{decodeURIComponent(QnA.question)}</Text>
-            {answers.map(el => (
-                <Button {...el} color={isAnswered ? (el.key === 3 ? 'green' : 'red') : ''}></Button>
-            ))}
+        <View style={_styles.container}>
+            <View style={_styles.questionContainer}>
+                <Text style={_styles.question}>
+                    {decodeURIComponent(QnA.question)}
+                </Text>
+            </View>
+            <View style={_styles.answersContainer}>
+                {answers.map(el => (
+                    <Pressable
+                        key={el.key}
+                        onPress={el.onPress}
+                        disabled={isAnswered}
+                        android_ripple={{ borderless: false }}
+                        style={isAnswered ? (el.key === 3 ? _styles.correct : _styles.incorrect) : _styles.normal}>
+                        <Text style={_styles.answer}>
+                            {el.title}
+                        </Text>
+                    </Pressable>
+                ))}
+            </View>
+
         </View>
     );
 }
@@ -36,4 +52,54 @@ QuestionScreen.propTypes = {
 
 const _styles = StyleSheet.create({
 
+    container: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        margin: 10
+    },
+    questionContainer: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    question: {
+        margin: 12,
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    answersContainer: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: 'center',
+        alignItems: 'stretch'
+    },
+    answer: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    normal: {
+        backgroundColor: '#aaa',
+        margin: 6,
+        padding: 12,
+        paddingStart: 20,
+        paddingEnd: 20,
+        borderRadius: 32
+    },
+    correct: {
+        backgroundColor: 'green',
+        margin: 6,
+        padding: 12,
+        paddingStart: 20,
+        paddingEnd: 20,
+        borderRadius: 32
+    },
+    incorrect: {
+        backgroundColor: 'red',
+        margin: 6,
+        padding: 12,
+        paddingStart: 20,
+        paddingEnd: 20,
+        borderRadius: 32
+    }
 });
